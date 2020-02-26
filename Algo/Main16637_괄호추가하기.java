d8ff59f4d6055e6e8616c5f8efa68ec3da8f1fc5
CJSONG <cjsonghae@naver.com>
2020-02-26 19:17:09 +0900

    문제풀이

---
+++ Algo/Main16637_괄호추가하기.java
@@ -0,0 +1,42 @@
+package test;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+ 
+public class Main16637_°ýÈ£Ãß°¡ÇÏ±â {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        input = br.readLine().toCharArray();
+        
+        dfs(1, input[0]-'0', 0, false);
+        System.out.println(max);
+    }
+    static int N;
+    static char[] input;
+    static int max = Integer.MIN_VALUE;
+    static void dfs(int op_i, int val, int pre_val, boolean wasBrace) {
+        
+        if(op_i >= N) {
+            if(max < val)
+                max = val;
+            return;
+        }
+        // ÇöÀç °ýÈ£ x
+        dfs(op_i+2, cal(val, input[op_i], input[op_i+1]-'0'), val, false);
+        
+        // ÇöÀç °ýÈ£ Ä¥ ¼ö ÀÖ³ª? µÇ¸é Ä§ 
+        if(op_i > 1 && !wasBrace) {
+            dfs(op_i+2, cal(pre_val, input[op_i-2], cal(input[op_i-1]-'0', input[op_i], input[op_i+1]-'0')),
+                    0, true);
+        }
+        
+    }
+    static int cal(int a, char op, int b) {
+        if(op == '+') return a + b;
+        else if(op == '-') return a - b;
+        else return a * b;
+    }
+}
---
+++ Algo/Main2437_저울.java
@@ -0,0 +1,35 @@
+package test;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main2437_Àú¿ï {
+	private static int N;
+	private static int[] arr;
+	private static boolean flag;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N];
+
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		int number = 0;
+		Arrays.sort(arr);
+		int min = 1;
+		for (int i = 0; i < N; i++) {
+			if (min < arr[i]) {
+				break;
+			}
+			min += arr[i];
+		}
+		System.out.println(min);
+
+	}// end of main
+
+}// end of class
